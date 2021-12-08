1b56836e0d96740af7b6ee68d87f59ba35dd7902
wooko5 <wooko5@naver.com>
2021-12-08 20:37:23 +0900

    feat: IngredientConverter 추가

--- src/main/java/com/shake_match/alchomist/ingredient/converter/IngredientConverter.java
+++ src/main/java/com/shake_match/alchomist/ingredient/converter/IngredientConverter.java
@@ -1,4 +1,16 @@
 package com.shake_match.alchomist.ingredient.converter;
 
+import com.shake_match.alchomist.ingredient.Ingredient;
+import com.shake_match.alchomist.ingredient.dto.IngredientRequest;
+import org.springframework.stereotype.Component;
+
+@Component
 public class IngredientConverter {
+
+    public Ingredient converterIngredient(IngredientRequest request) { // dto -> entity
+        return Ingredient.builder()
+                .name(request.getIngredientName())
+                .cocktails(request.getCocktails())
+                .build();
+    }
 }
