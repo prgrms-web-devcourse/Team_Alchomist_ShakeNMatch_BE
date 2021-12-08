3d2a617eb1d34583a9bd2b9014bc1664b8c485c0
wooko5 <wooko5@naver.com>
2021-12-08 20:37:11 +0900

    feat: NOT_EXIST_COCKTAIL 에러메시지 추가

--- src/main/java/com/shake_match/alchomist/global/ErrorCode.java
+++ src/main/java/com/shake_match/alchomist/global/ErrorCode.java
@@ -15,6 +15,7 @@ public enum ErrorCode {
     INVALID_POST_STATUS("존재하지 않는 게시글 상태 입니다."),
     INVALID_IMAGE_URL("이미지의 URL 형식이 맞지 않습니다."),
     INVALID_IMAGE_TYPE("이미지의 종류 형식이 맞지 않습니다."),
+    NOT_EXIST_COCKTAIL("존재하지 않는 칵테일입니다"),
     NOT_EXIST_IMAGE("존재하지 않는 이미지입니다"),
     NOT_EXIST_BADGE("존재하지 않는 배지입니다"),
     UNSUPPORTED_MEDIA_TYPE("지원하지 않는 미디어 타입입니다."),
