ae61e519a2e0a0e98a8a4ffcf8a9458bae61cce9
wooko5 <wooko5@naver.com>
2021-12-08 20:37:30 +0900

    feat: IngredientController 추가

--- src/main/java/com/shake_match/alchomist/ingredient/controller/IngredientController.java
+++ src/main/java/com/shake_match/alchomist/ingredient/controller/IngredientController.java
@@ -1,4 +1,31 @@
 package com.shake_match.alchomist.ingredient.controller;
 
+import com.shake_match.alchomist.global.ApiResponse;
+import com.shake_match.alchomist.global.NotFoundException;
+import com.shake_match.alchomist.ingredient.dto.IngredientResponse;
+import com.shake_match.alchomist.ingredient.service.IngredientService;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class IngredientController {
+    private final IngredientService ingredientService;
+
+    public IngredientController(IngredientService ingredientService) {
+        this.ingredientService = ingredientService;
+    }
+
+    @GetMapping("/ingredient/{id}")
+    public ApiResponse<List<IngredientResponse>> findAllById(Pageable pageable, @PathVariable("id") Long ingredientId) throws NotFoundException {
+        return ApiResponse.ok(ingredientService.findAllById(pageable, ingredientId));
+    }
+
+    @GetMapping("/ingredient/{name}")
+    public ApiResponse<List<IngredientResponse>> findAllByName(Pageable pageable, @PathVariable("name") String ingredientName) throws NotFoundException {
+        return ApiResponse.ok(ingredientService.findAllByName(pageable, ingredientName));
+    }
 }
