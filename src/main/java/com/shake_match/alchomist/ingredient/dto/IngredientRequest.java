960e9d4d718c3e2ce6f2341ad9e682f19cfb941f
wooko5 <wooko5@naver.com>
2021-12-08 20:37:46 +0900

    feat: IngredientDto 추가

--- src/main/java/com/shake_match/alchomist/ingredient/dto/IngredientRequest.java
+++ src/main/java/com/shake_match/alchomist/ingredient/dto/IngredientRequest.java
@@ -1,14 +1,25 @@
 package com.shake_match.alchomist.ingredient.dto;
 
+import com.shake_match.alchomist.cocktail.domain.Cocktail;
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
 public class IngredientRequest {
     private Long ingredientId;
-    private String name;
+    private String ingredientName;
+    private List<Cocktail> cocktails;
 
     public Long getIngredientId() {
         return this.ingredientId;
     }
 
-    public String getName() {
-        return this.name;
+    public String getIngredientName() {
+        return this.ingredientName;
+    }
+
+    public List<Cocktail> getCocktails() {
+        return this.cocktails;
     }
 }
