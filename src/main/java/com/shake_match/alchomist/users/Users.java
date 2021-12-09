535d00f402dbbe100ad7d68385cef7f5e18c057d
Rudolf <stylish0631@gmail.com>
2021-12-09 18:51:36 +0900

    feat: User 기능 개발

--- src/main/java/com/shake_match/alchomist/users/Users.java
+++ src/main/java/com/shake_match/alchomist/users/Users.java
@@ -12,7 +12,16 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
 @Entity(name = "users")
 public class Users extends BaseEntity {
 
@@ -35,6 +44,7 @@ public class Users extends BaseEntity {
     @Column(nullable = false)
     boolean gender;
 
+    @Size(min = 20)
     @Column(nullable = false)
     int age;
 
@@ -50,5 +60,15 @@ public class Users extends BaseEntity {
     @OneToMany
     List<Cocktail> cocktails = new ArrayList<>();
 
+    public void addCocktails(Cocktail cocktail) {
+        if(!this.getCocktails().contains(cocktail)){
+            cocktails.add(cocktail);
+            cocktail.addLikes(true);
+        }
+        else {
+            cocktails.remove(cocktail);
+            cocktail.addLikes(false);
+        }
+    }
 
 }
---
+++ src/main/java/com/shake_match/alchomist/users/dto/response/UserLikeResponse.java
@@ -0,0 +1,13 @@
+package com.shake_match.alchomist.users.dto.response;
+
+import com.shake_match.alchomist.users.Users;
+
+public class UserLikeResponse {
+
+    Users user;
+
+    public UserLikeResponse(Users users) {
+        this.user = users;
+    }
+
+}
