2b496c591092f7c76c7baa8dfd003b4841b51176
wooko5 <wooko5@naver.com>
2021-12-10 14:31:23 +0900

    chore: IngredientController에 @RequestMapping 추가

--- src/main/java/com/shake_match/alchomist/ingredient/controller/IngredientController.java
+++ src/main/java/com/shake_match/alchomist/ingredient/controller/IngredientController.java
@@ -7,11 +7,13 @@ import com.shake_match.alchomist.ingredient.service.IngredientService;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/ingredient")
 public class IngredientController {
     private final IngredientService ingredientService;
 
@@ -19,12 +21,12 @@ public class IngredientController {
         this.ingredientService = ingredientService;
     }
 
-    @GetMapping("/ingredient/{id}")
+    @GetMapping("/{id}")
     public ApiResponse<List<IngredientDetailResponse>> findAllById(Pageable pageable, @PathVariable("id") Long ingredientId) throws NotFoundException {
         return ApiResponse.ok(ingredientService.findAllById(pageable, ingredientId));
     }
 
-    @GetMapping("/ingredient/{name}")
+    @GetMapping("/{name}")
     public ApiResponse<List<IngredientDetailResponse>> findAllByName(Pageable pageable, @PathVariable("name") String ingredientName) throws NotFoundException {
         return ApiResponse.ok(ingredientService.findAllByName(pageable, ingredientName));
     }
