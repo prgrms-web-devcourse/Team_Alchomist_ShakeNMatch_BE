7ae28f43ccea99cd718d2a8a524c06fba30a2b3b
Rudolf <stylish0631@gmail.com>
2021-12-14 16:58:23 +0900

    feat: 유저 정보 수정 기능 추가

--- src/main/java/com/shake_match/alchomist/users/controller/UserController.java
+++ src/main/java/com/shake_match/alchomist/users/controller/UserController.java
@@ -3,6 +3,7 @@ package com.shake_match.alchomist.users.controller;
 import com.shake_match.alchomist.global.ApiResponse;
 import com.shake_match.alchomist.users.dto.request.UserBookmarkRequest;
 import com.shake_match.alchomist.users.dto.request.UserRequest;
+import com.shake_match.alchomist.users.dto.request.UserUpdateRequest;
 import com.shake_match.alchomist.users.dto.response.UserBookmarkResponse;
 import com.shake_match.alchomist.users.dto.response.UserDetailResponse;
 import com.shake_match.alchomist.users.dto.response.UserLikeResponse;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +34,14 @@ public class UserController {
     public ApiResponse<UserDetailResponse> saveUser(@RequestBody @Valid UserRequest userRequest) {
         UserDetailResponse userResponse = userService.addUser(userRequest);
         return ApiResponse.ok(userResponse);
+    }
+
+    @PutMapping("/info/{userId}") // 회원정보 수정
+    public ApiResponse<String> updateUserInfo(@PathVariable("userId") Long id,
+                                              @RequestBody UserUpdateRequest userUpdateRequest)
+        throws Exception {
+        userService.updateById(id, userUpdateRequest);
+        return ApiResponse.ok("회원정보가 수정되었습니다");
     }
 
     @GetMapping("/{userId}") // 유저 디테일 조회
@@ -63,10 +73,10 @@ public class UserController {
     }
 
     @DeleteMapping("/bookmark") // 북마크 삭제
-    public ApiResponse<String> deleteUserBookmark(@RequestBody UserBookmarkRequest userBookmarkRequest) {
+    public ApiResponse<String> deleteUserBookmark(
+        @RequestBody UserBookmarkRequest userBookmarkRequest) {
         userService.deleteBookmark(userBookmarkRequest);
         return ApiResponse.ok("bookmark Deleted successfully");
     }
-
 
 }
