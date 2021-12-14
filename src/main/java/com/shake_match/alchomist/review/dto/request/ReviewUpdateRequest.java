63edac3d4ede3f5d6fc6565038c62209d4c07ee4
wooko5 <wooko5@naver.com>
2021-12-15 07:52:43 +0900

    chore: ReviewUpdateRequest의 reviewId 제거

--- src/main/java/com/shake_match/alchomist/review/dto/request/ReviewUpdateRequest.java
+++ src/main/java/com/shake_match/alchomist/review/dto/request/ReviewUpdateRequest.java
@@ -1,15 +1,9 @@
 package com.shake_match.alchomist.review.dto.request;
 
 public class ReviewUpdateRequest {
-
-    private Long reviewId;
     private int rating;
     private String description;
     private String type;
-
-    public Long getReviewId() {
-        return reviewId;
-    }
 
     public int getRating() {
         return rating;
