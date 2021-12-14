62467fd8382d7a12e4e769b8e56dcdde9f08f80e
Rudolf <stylish0631@gmail.com>
2021-12-14 17:46:46 +0900

    feat: cors 풀기

--- src/main/java/com/shake_match/alchomist/global/WebMvcConfig.java
+++ src/main/java/com/shake_match/alchomist/global/WebMvcConfig.java
@@ -9,6 +9,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**").allowedOrigins("*");
+        registry.addMapping("/**").allowedOrigins("*");
     }
 }
